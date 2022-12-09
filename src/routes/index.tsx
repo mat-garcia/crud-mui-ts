a1dd1e4f649254e01fa725e0f2e3eae53572b7f3
Mateus Garcia <mateussoap@gmail.com>
2022-12-08 22:13:10 -0300

    instalando MUI 5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@mui/material';
 import {Routes, Route, Navigate} from 'react-router-dom'
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/home' element={<p> Pagina Incial</p>}/>
+            <Route path='/home' element={<Button variant='contained' > Click me! </Button>}/>
 
             <Route path="*" element={<Navigate to="/home"/>}/>
         </Routes>
